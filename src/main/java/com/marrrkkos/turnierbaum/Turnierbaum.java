b251c382838f82ebcb131264be0a4f36901f227c
Lars Erhardt <Nerd2201@protonmail.com>
2019-11-17 00:25:15 +0100

    Basic Ui

--- src/main/java/com/marrrkkos/turnierbaum/Turnierbaum.java
+++ src/main/java/com/marrrkkos/turnierbaum/Turnierbaum.java
@@ -1,16 +1,23 @@
 package com.marrrkkos.turnierbaum;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Turnierbaum extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
+        Scene scene = new Scene(root, 1920, 1080);
+        primaryStage.setTitle("Turnierbaum");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
+    public static void main(String[] args) {
+        launch(args);
     }
 }
